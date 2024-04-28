693518fdbca6ba9b38db02f2111bece75fc68fa3
Samichhya88 <165799516+Samichhya88@users.noreply.github.com>
2024-04-28 22:03:50 +0545

    file controller
    file controller

---
+++ Controllers/BlogPostController.cs
@@ -0,0 +1,144 @@
+﻿using Azure;
+using BisleriumBloggers.DTOs.Base;
+using BisleriumBloggers.DTOs.Blog;
+using BisleriumBloggers.Interfaces.Repositories.Base;
+using BisleriumBloggers.Interfaces.Services;
+using BisleriumBloggers.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Reflection.Metadata;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace BisleriumBloggers.Controllers;
+[Authorize]
+[ApiController]
+[Route("api/blog")]
+public class BlogController : Controller
+{
+    private readonly IUserService _userService;
+    private readonly IGenericRepository _genericRepository;
+
+    public BlogController(IGenericRepository genericRepository, IUserService userService)
+    {
+        _genericRepository = genericRepository;
+        _userService = userService;
+    }
+
+
+
+    /*This method creates a new blog post, assigns it to the current user, and returns a success response upon insertion.*/
+
+    [HttpPost("create-blog")]
+    public IActionResult CreateBlog(BlogCreateDto blog)
+    {
+        var userId = _userService.UserId;
+
+        var user = _genericRepository.GetById<User>(userId);
+
+        var blogModel = new Blog()
+        {
+            Title = blog.Title,
+            Body = blog.Body,
+            Location = blog.Location,
+            Reaction = blog.Reaction,
+            BlogImages = blog.Images?.Select(x => new BlogImage()
+            {
+                ImageURL = x,
+                IsActive = true,
+                CreatedAt = DateTime.Now,
+                CreatedBy = user.Id
+            }).ToList(),
+            CreatedAt = DateTime.Now,
+            CreatedBy = user.Id,
+        
+        
+        };
+
+
+        _genericRepository.Insert(blogModel);
+
+        return Ok(new ResponseDto<object>()
+        {
+            Status = "Success",
+            StatusCode = HttpStatusCode.OK,
+            TotalCount = 1,
+            Message = "Successfully Inserted",
+            Result = true
+        });
+    }
+
+
+    /*This method updates a blog post, creates a log of the update, and returns a success response upon successful update*/
+    [HttpPatch("update-blog")]
+    public IActionResult UpdateBlog(BlogDetailsDto blog)
+    {
+        var userId = _userService.UserId;
+
+        var user = _genericRepository.GetById<User>(userId);
+
+        var blogModel = _genericRepository.GetById<Blog>(blog.Id);
+
+        var blogLog = new BlogLog()
+        {
+            BlogId = blogModel.Id,
+            Title = blogModel.Title,
+            Location = blogModel.Location,
+            Reaction = blogModel.Reaction,
+            CreatedAt = DateTime.Now,
+            CreatedBy = user.Id,
+            Body = blogModel.Body,
+            IsActive = false
+        };
+
+        _genericRepository.Insert(blogLog);
+
+        blogModel.Title = blog.Title;
+        blogModel.Body = blog.Body;
+        blogModel.Location = blog.Location;
+        blogModel.Reaction = blog.Reaction;
+
+        blogModel.LastModifiedAt = DateTime.Now;
+        blogModel.LastModifiedBy = user.Id;
+
+        _genericRepository.Update(blogModel);
+
+        return Ok(new ResponseDto<object>()
+        {
+            Status = "Success",
+            StatusCode = HttpStatusCode.OK,
+            TotalCount = 1,
+            Message = "Successfully Updated",
+            Result = true
+        });
+    }
+
+
+    /*This method soft deletes a blog post, updating its status and recording deletion details,
+    and returns a success response upon completion*/
+    [HttpDelete("delete-blog/{blogId:int}")]
+    public IActionResult DeletePost(int blogId)
+    {
+        var userId = _userService.UserId;
+
+        var user = _genericRepository.GetById<User>(userId);
+
+        var blogModel = _genericRepository.GetById<Blog>(blogId);
+
+        blogModel.IsActive = false;
+        blogModel.DeletedAt = DateTime.Now;
+        blogModel.DeletedBy = user.Id;
+
+        _genericRepository.Update(blogModel);
+
+        return Ok(new ResponseDto<object>()
+        {
+            Status = "Success",
+            StatusCode = HttpStatusCode.OK,
+            TotalCount = 1,
+            Message = "Successfully Updated",
+            Result = true
+        });
+    }
+}
---
+++ Controllers/FileControlle.cs
@@ -0,0 +1,98 @@
+﻿// ! Importing necessary namespaces
+
+using BisleriumBloggers.DTOs.Base;
+using BisleriumBloggers.DTOs.Upload;
+using BisleriumBloggers.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using System.Net;
+
+
+// Defining the namespace and class for file upload controller
+namespace BisleriumBloggers.Controllers;
+
+// Attributes for API controller and route
+[ApiController]
+[Route("api/file-upload")]
+public class FileUploadController : Controller
+{
+    private readonly IFileUploadService _fileUploadService;
+
+// Constructor injection for file upload service
+    public FileUploadController(IFileUploadService fileUploadService)
+    {
+        _fileUploadService = fileUploadService;
+    }
+
+    // POST method for uploading files
+    [HttpPost]
+    public IActionResult UploadFile([FromForm] UploadDto uploads)
+    {
+         // Checking if the file path is a valid integer
+        if (!int.TryParse(uploads.FilePath, out int filePathIndex))
+        {
+            // Returning a bad request response with error message
+            return BadRequest(new ResponseDto<object>()
+            {
+                Message = "Invalid File Path.",
+                StatusCode = HttpStatusCode.BadRequest,
+                TotalCount = 0,
+                Status = "Bad Request",
+                Result = false
+            });
+        }
+
+// Determining file paths based on index
+        var filePaths = filePathIndex switch
+        {
+            1 => Constants.Constants.FilePath.UsersImagesFilePath,
+            2 => Constants.Constants.FilePath.BlogsImagesFilePath,
+            _ => ""
+        };
+
+// Checking if file paths are empty or null
+        if (string.IsNullOrEmpty(filePaths))
+        {
+            // Returning a bad request response with error message
+            return BadRequest(new ResponseDto<object>()
+            {
+                Message = "Invalid File Path.",
+                StatusCode = HttpStatusCode.BadRequest,
+                TotalCount = 0,
+                Status = "Bad Request",
+                Result = false
+            });
+        }
+
+// Checking if file size is greater than 3MB
+        const long maxSize = 3 * 1024 * 1024;
+
+        if (uploads.Files.Any(upload => upload.Length > maxSize))
+        {
+            // Returning a bad request response with error message
+            return BadRequest(new ResponseDto<object>()
+            {
+                Message = "Invalid File Size.",
+                StatusCode = HttpStatusCode.BadRequest,
+                TotalCount = 0,
+                Status = "Bad Request",
+                Result = false
+            });
+        }
+// Uploading files and getting file names
+        var fileNames = uploads.Files.Select(file => _fileUploadService.UploadDocument(filePaths, file)).ToList();
+
+// Creating a response with uploaded file names
+        var response = new ResponseDto<List<string>>()
+        {
+            Message = "File successfully uploaded.",
+            Result = fileNames,
+            StatusCode = HttpStatusCode.OK,
+            Status = "Success",
+            TotalCount = fileNames.Count
+        };
+  // Returning success response with uploaded file names
+        return Ok(response);
+    }
+}
